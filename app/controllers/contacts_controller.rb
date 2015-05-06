565a09fb5013f02e67d69d2b125ffb514352981a
Ron Marconi <marconironald@hotmail.com>
2015-05-06 08:18:23 +0000

    updated the create action in the contacts controller by whitelisting the params and adding flashes dependant on form saving or not

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,5 +4,22 @@ class ContactsController < ApplicationController
   end
   
   def create
+    @contact = Contact.new(contact_params)
+    
+    if @contact.save
+      flash[:success] = 'Thank You, we will respond shortly.'
+      redirect_to new_contact_path
+    else
+      flash[:danger] = 'Something went wrong! please ensure name and email fields are filled in'
+      redirect_to new_contact_path
+    end
+      
+  end
+  
+  
+  private
+  
+  def contact_params
+    params.require(:contact).permit(:name,:email,:comments)
   end
 end
